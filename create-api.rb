33e9a505edbdf4798e151e3211a5cf698a8dcda6
Marko Opačić <marko.opacic+github@flabbergast.agency>
2025-12-19 17:55:57 +0100

    fix sha256 sum

--- create-api.rb
+++ create-api.rb
@@ -2,7 +2,7 @@ class CreateApi < Formula
   desc ""
   homepage "https://github.com/FlabbergastAgency/CreateAPI"
   url "https://github.com/FlabbergastAgency/CreateAPI/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "8fad9f411884667bcd35467f815b0b3d81774f35392901c23517cf5658446b7c"
   license "MIT"
 
   depends_on :xcode => ["14.0", :build]
